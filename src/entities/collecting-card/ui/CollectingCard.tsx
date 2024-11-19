4391b96207ece00a6e45ee96a02d74bf45d7378a
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2024-11-19 13:24:13 +0300

    check swiper bug #18

--- src/entities/collecting-card/ui/CollectingCard.tsx
+++ src/entities/collecting-card/ui/CollectingCard.tsx
@@ -95,33 +95,34 @@ export const CollectingCard: FC<ICollectingCard> = ({
 
 					<span className={s.date}>{date}</span>
 				</div>
+				<Fancybox className={s.swiperWrapper}>
+					<Swiper
+						onSwiper={swiper => {
+							setSwiper(swiper);
+						}}
+						slidesPerView={1}
+						modules={[Pagination]}
+						pagination={{
+							el: pagination.current,
+							clickable: true
+						}}
+					>
+						{imgs.map((img, index) => (
+							<SwiperSlide key={index}>
+								<img src={img} alt='' data-fancybox='collecting' />
+								{/* <Image
+									paddingBottom='71%'
+									src={img}
+									alt='slide'
+									className={s.image}
+									fancybox='collecting'
+								/> */}
+							</SwiperSlide>
+						))}
+					</Swiper>
+					<div ref={pagination}></div>
+				</Fancybox>
 			</div>
-			<Fancybox className={s.swiperWrapper}>
-				<Swiper
-					onSwiper={swiper => {
-						setSwiper(swiper);
-					}}
-					slidesPerView={1}
-					modules={[Pagination]}
-					pagination={{
-						el: pagination.current,
-						clickable: true
-					}}
-				>
-					{imgs.map((img, index) => (
-						<SwiperSlide key={index}>
-							<Image
-								paddingBottom='71%'
-								src={img}
-								alt='slide'
-								className={s.image}
-								fancybox='collecting'
-							/>
-						</SwiperSlide>
-					))}
-				</Swiper>
-				<div ref={pagination}></div>
-			</Fancybox>
 			<div className={s.collectingCardBottom}>
 				<a href='#' className={s.collectingCardBottomContent}>
 					<p className={s.title}>{cropText(title, 52)}</p>
